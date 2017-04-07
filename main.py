5ff26abc478f6aae9dc6aa08cba4916008bd1413
ukurumba <ukurumba@u.rochester.edu>
2017-04-06 20:28:59 -0400

    made command line arguments fit

--- main.py
+++ main.py
@@ -4,30 +4,45 @@ import xml.etree.ElementTree as ET
 import sys
 import os
 
+
+
+
+
+
 if sys.argv[1][-4:] == '.xml':
 	tree = ET.parse(sys.argv[1])
 	tree.write('temp.xml')
 	reader = p.XMLBIFReader('temp.xml')
 	os.remove('temp.xml')
 
-elif sys.argv[1][-4] == '.bif':
+elif sys.argv[1][-4:] == '.bif':
 	reader = p.BIFReader(sys.argv[1])
 
 else:
 	raise NameError("File I/O Failed. Please ensure file format is .xml or .bif and correct order of arguments. See README for format/examples")
 
+add_on = 0
+if len(sys.argv) / 2 == float(len(sys.argv)//2): # if even
+	add_on = 1
+	
+	
+
 E = []
 e = []
 for i in range(len(sys.argv)):
-	if i > 2:
-		if i/2 == float(i//2): #even arguments
+	if i > 2 + add_on:
+		if (i+ add_on)/2 == float((i+add_on)//2): #even arguments
 			e.append(sys.argv[i])
 		else:
 			E.append(sys.argv[i])
 
 net = bn.Bayesian_Network(reader.get_variables(),reader.get_parents(),reader.get_cpd(),reader.get_states())
-estimated = bn.likelihood_weighting(sys.argv[2],E,e,net,1000)
-actual = bn.enumeration_ask(sys.argv[2],E,e,net)
-print('Estimate: ',estimated)
-print('Actual: ',actual)
+
+if add_on == 1:
+	estimated = bn.likelihood_weighting(sys.argv[3],E,e,net,int(sys.argv[2]))
+	print('Estimate: ',estimated)
+
+else: 
+	actual = bn.enumeration_ask(sys.argv[2],E,e,net)
+	print('Actual: ',actual)
 
